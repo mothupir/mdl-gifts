64389ad34d78fbfd28b30f02ab3050dc91291d39
mothupir <mothupir@bbd.co.za>
2025-01-13 10:14:56 +0200

    added dashboard and updated config

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Theme from '@primeng/themes/lara';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Theme,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: true
+        }
+      }
+    })
+  ]
 };
